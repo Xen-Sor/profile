0f3e8b889e517ad8f0c30ae1227d0a0d83745642
Xen-Sor <kseniia.m.sorokina@gmail.com>
2024-05-23 22:34:54 +0700

    Add blog and registration

--- django/TMlandin/TMlandin/urls.py
+++ django/TMlandin/TMlandin/urls.py
@@ -3,5 +3,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("home/", include("intro.urls")),
+    path("blog/", include("blog.urls")),
+    path("registration/", include("registration.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("admin/", admin.site.urls),
 ]
